38617acfb1fadc911a6e774a7a226516d522100f
nmcb <marco@splatter.nl>
2018-10-15 15:33:57 +0200

    remove unused trait

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,13 +3,12 @@ import config.Config
 import db.Database
 import fs2.{Stream, StreamApp}
 import fs2.StreamApp.ExitCode
-import org.http4s.dsl.Http4sDsl
 import org.http4s.server.blaze.BlazeBuilder
 import repository.TodoRepository
 import service.TodoService
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object Server extends StreamApp[IO] with Http4sDsl[IO] {
+object Server extends StreamApp[IO] {
   def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
     for {
       config <- Stream.eval(Config.load())
