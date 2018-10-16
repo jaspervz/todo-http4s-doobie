1a1cdb065f5954aef887b30c1f6ef9bd1dd74b0e
Marco Borst <marco.borst@ing.com>
2018-10-16 14:43:21 +0200

    service chore

--- src/main/scala/model/package.scala
+++ src/main/scala/model/package.scala
@@ -1,16 +1,23 @@
+import io.circe.{Decoder, Encoder}
+
 package object model {
   
   abstract sealed class Importance(val value: String)
-  case object High extends Importance("high")
+  case object High   extends Importance("high")
   case object Medium extends Importance("medium")
-  case object Low extends Importance("low")
+  case object Low    extends Importance("low")
 
   object Importance {
     private def values = Set(High, Medium, Low)
 
-    def unsafeFromString(value: String): Importance = {
+    def unsafeFromString(value: String): Importance =
       values.find(_.value == value).get
-    }
+
+    implicit val encodeImportance: Encoder[Importance] =
+      Encoder.encodeString.contramap[Importance](_.value)
+
+    implicit val decodeImportance: Decoder[Importance] =
+      Decoder.decodeString.map[Importance](Importance.unsafeFromString)
   }
 
   case class Contact(id: Option[Long], description: String, importance: Importance)
--- src/main/scala/service/ContactService.scala
+++ src/main/scala/service/ContactService.scala
@@ -1,57 +1,64 @@
 package service
 
 import cats.effect.IO
-import model.{Importance, Contact, ContactNotFound}
-import org.http4s.{HttpService, MediaType, Uri}
+import model.{Contact, ContactNotFound}
+import org.http4s.{HttpService, MediaType, Request, Response, Uri}
 import org.http4s.dsl.Http4sDsl
 import org.http4s.circe._
 import repository.ContactRepository
 import io.circe.generic.auto._
 import io.circe.syntax._
 import fs2.Stream
-import io.circe.{Decoder, Encoder}
 import org.http4s.headers.{Location, `Content-Type`}
 
 class ContactService(repository: ContactRepository) extends Http4sDsl[IO] {
-  private implicit val encodeImportance: Encoder[Importance] = Encoder.encodeString.contramap[Importance](_.value)
-
-  private implicit val decodeImportance: Decoder[Importance] = Decoder.decodeString.map[Importance](Importance.unsafeFromString)
 
   val service = HttpService[IO] {
-    case GET -> Root / "contacts" =>
-      Ok(Stream("[") ++ repository.getContacts.map(_.asJson.noSpaces).intersperse(",") ++ Stream("]"), `Content-Type`(MediaType.`application/json`))
-
-    case GET -> Root / "contacts" / LongVar(id) =>
-      for {
-        getResult <- repository.getContact(id)
-        response <- ContactResult(getResult)
-      } yield response
-
-    case req @ POST -> Root / "contacts" =>
-      for {
-        contact <- req.decodeJson[Contact]
-        createdContact <- repository.createContact(contact)
-        response <- Created(createdContact.asJson, Location(Uri.unsafeFromString(s"/contacts/${createdContact.id.get}")))
-      } yield response
-
-    case req @ PUT -> Root / "contacts" / LongVar(id) =>
-      for {
-        contact <-req.decodeJson[Contact]
-        updateResult <- repository.updateContact(id, contact)
-        response <- ContactResult(updateResult)
-      } yield response
-
-    case DELETE -> Root / "contacts" / LongVar(id) =>
-      repository.deleteContact(id).flatMap {
-        case Left(ContactNotFound()) => NotFound()
-        case Right(_) => NoContent()
-      }
+    case req @ POST -> Root / "contacts"                =>  serveCreatedContact(req)
+    case req @ PUT  -> Root / "contacts" / LongVar(id)  =>  serveUpdatedContact(id, req)
+    case GET        -> Root / "contacts" / LongVar(id)  =>  serveContactById(id)
+    case DELETE     -> Root / "contacts" / LongVar(id)  =>  serveDeletedContact(id)
+    case GET        -> Root / "contacts"                =>  serveAllContacts
   }
 
-  private def ContactResult(result: Either[ContactNotFound, Contact]) = {
+  def serveCreatedContact(req: Request[IO]): IO[Response[IO]] =
+    for {
+      contact        <- req.decodeJson[Contact]
+      createdContact <- repository.createContact(contact)
+      response       <- Created(createdContact.asJson, Location(Uri.unsafeFromString(s"/contacts/${createdContact.id.get}")))
+    } yield response
+
+  def serveUpdatedContact(id: Long, req: Request[IO]): IO[Response[IO]] =
+    for {
+      contact      <- req.decodeJson[Contact]
+      updateResult <- repository.updateContact(id, contact)
+      response     <- responseFromResult(updateResult)
+    } yield response
+
+  def serveContactById(id: Long): IO[Response[IO]] =
+    for {
+      getResult <- repository.getContact(id)
+      response  <- responseFromResult(getResult)
+    } yield response
+
+  def serveDeletedContact(id: Long): IO[Response[IO]] =
+    repository.deleteContact(id).flatMap {
+      case Left(ContactNotFound()) => NotFound()
+      case Right(_)                => NoContent()
+    }
+
+  def serveAllContacts: IO[Response[IO]] =
+    Ok(
+      Stream("[")
+        ++ repository.getContacts.map(_.asJson.noSpaces).intersperse(",")
+        ++ Stream("]"),
+      `Content-Type`(MediaType.`application/json`)
+    )
+
+  private def responseFromResult(result: Either[ContactNotFound, Contact]) = {
     result match {
       case Left(ContactNotFound()) => NotFound()
-      case Right(contact) => Ok(contact.asJson)
+      case Right(contact)          => Ok(contact.asJson)
     }
   }
 }
