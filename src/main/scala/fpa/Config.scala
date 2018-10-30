124e916d9ec87f72167b2bde49c454d1912a3414
nmcb <marco@splatter.nl>
2018-10-30 09:29:10 +0100

    add request response logging middleware and configuration

--- src/main/scala/fpa/Config.scala
+++ src/main/scala/fpa/Config.scala
@@ -7,7 +7,10 @@ case class ServerConfig(host: String, port: Int)
 
 case class DatabaseConfig(driver: String, url: String, user: String, password: String)
 
-case class Config(server: ServerConfig, database: DatabaseConfig)
+case class LoggingConfig(logHeaders: Boolean, logBody: Boolean)
+
+case class Config(server: ServerConfig, database: DatabaseConfig, logging: LoggingConfig)
+
 
 object Config {
 
